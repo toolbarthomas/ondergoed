3d703d520f53fe9184cd97eb9867564ebfe6b1a0
Thomas van der Velde <contact@toolbarthomas.net>
2025-11-04 21:49:13 +0100

    chore(node): Update exports

--- node/build.mjs
+++ node/build.mjs
@@ -1,29 +1,27 @@
-import esbuild from "esbuild";
-import { parse } from "@toolbarthomas/argumentje";
+import esbuild from 'esbuild'
+import { parse } from '@toolbarthomas/argumentje'
 
 /**
  * Compiles the required modules.
  */
-(async () => {
-  const { minify, format } = parse();
-  const extension = format === "cjs" ? ".cjs" : format === "esm" ? "mjs" : "js";
+;(async () => {
+  const { minify, format } = parse()
+  const extension = format === 'cjs' ? 'cjs' : format === 'esm' ? 'mjs' : 'js'
 
   const defaults = {
     bundle: true,
-    entryPoints: ["./src/index.ts"],
+    entryPoints: ['./src/index.ts'],
     external: [],
     minify,
     keepNames: true,
     metafile: false,
-    outdir: "dist",
+    outdir: 'dist',
     format,
-    platform: "node",
-    outExtension: { ".js": minify ? `.min.${extension}` : `.${extension}` },
-  };
+    platform: 'node',
+    outExtension: { '.js': minify ? `.min.${extension}` : `.${extension}` }
+  }
 
-  esbuild
-    .build({ ...defaults, format: format === "cjs" ? "cjs" : "esm" })
-    .then(() => {
-      console.info(`Package compiled.`);
-    });
-})();
+  esbuild.build({ ...defaults, format: format === 'cjs' ? 'cjs' : 'esm' }).then(() => {
+    console.info(`Package compiled.`)
+  })
+})()
